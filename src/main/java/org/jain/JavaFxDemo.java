0ad43c9d947a56c6d644a4fa66b2a1e147c95997
PAYAL JAIN <PAYAL JAIN@LAPTOP-A729F3L0>
2024-12-26 16:17:44 +0530

    feat: maven/javafx demo
    * implemented stage(javafx)
    * maven

---
+++ src/main/java/org/jain/JavaFxDemo.java
@@ -0,0 +1,31 @@
+package org.jain;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class JavaFxDemo extends Application{
+	public static void main(String args[])
+	{
+		Application.launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Stage stage=new Stage();
+		
+		stage.setTitle("First Window");
+		stage.setFullScreen(true);
+		stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+		
+		Group actorgroup=new Group();
+		
+		Scene scene=new Scene(actorgroup, Color.ALICEBLUE);
+		
+		stage.setScene(scene);
+		stage.show();
+	}
+}
